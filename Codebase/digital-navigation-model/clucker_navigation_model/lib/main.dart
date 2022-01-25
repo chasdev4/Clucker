77511f7b2848e67c2809989d860c598113eb4f7a
Zachary Culpepper <zculp4579@gmail.com>
2022-01-24 19:39:14 -0600

    Clucker Prototype Nav Model
    First Draft

--- Codebase/digital-navigation-model/clucker_navigation_model/lib/main.dart
+++ Codebase/digital-navigation-model/clucker_navigation_model/lib/main.dart
@@ -1,115 +1,517 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+        title: 'Routes Practice',
+        initialRoute: '/SignIn',
+
+        routes: {
+          '/SignIn': (context) => const SignInScreen(),
+          '/SignUp': (context) => const SignUpScreen(),
+          '/Home': (context) => const HomeScreen(),
+          '/Search': (context) => const SearchScreen(),
+          '/Discover': (context) => const DiscoverScreen(),
+          '/Notification': (context) => const NotificationScreen(),
+          '/NewCluck': (context) => const NewCluckScreen(),
+          '/UserMenu': (context) => const UserMenuScreen(),
+          '/Profile': (context) => const ProfileScreen(),
+          //'/Settings': (context) => const SettingScreen(),
+          //'/Followers': (context) => const FollowersScreen(),
+          //'/Following': (context) => const FollowingScreen(),
+          //'/BlockedUsers': (context) => const BlockedUsersScreen(),
+
+        }
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign-In Screen'),
+        backgroundColor: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('Sign In'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/SignUp');
+                  },
+                  child: const Text('Sign Up'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Sign-Up Screen'),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('Sign Up'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class UserMenuScreen extends StatelessWidget {
+  const UserMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Menu'),
+        backgroundColor: Colors.yellowAccent,
+      ),
+      body: Column(
+        children: <Widget> [
+          Row(
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: const Text('Profile'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile Screen'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Column(
+        children: <Widget> [
+          Row(),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Screen'),
+        backgroundColor: Colors.red,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/UserMenu');
+                  },
+                  child: const Text('User Menu'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('H'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Discover');
+                  },
+                  child: const Text('D'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/NewCluck');
+                  },
+                  child: const Text('NC'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Search');
+                  },
+                  child: const Text('S'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Notification');
+                  },
+                  child: const Text('N'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        backgroundColor: Colors.green,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Profile');
+                  },
+                  child: const Text('Profile'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('H'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Discover');
+                  },
+                  child: const Text('D'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/NewCluck');
+                  },
+                  child: const Text('NC'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Search');
+                  },
+                  child: const Text('S'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Notification');
+                  },
+                  child: const Text('N'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Discover Screen'),
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/UserMenu');
+                  },
+                  child: const Text('User Menu'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('H'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Discover');
+                  },
+                  child: const Text('D'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/NewCluck');
+                  },
+                  child: const Text('NC'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Search');
+                  },
+                  child: const Text('S'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Notification');
+                  },
+                  child: const Text('N'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notification Screen'),
+        backgroundColor: Colors.deepPurple,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget> [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/UserMenu');
+                  },
+                  child: const Text('User Menu'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('H'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Discover');
+                  },
+                  child: const Text('D'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/NewCluck');
+                  },
+                  child: const Text('NC'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Search');
+                  },
+                  child: const Text('S'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Notification');
+                  },
+                  child: const Text('N'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NewCluckScreen extends StatelessWidget {
+  const NewCluckScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Cluck Screen'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget> [
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Home');
+                  },
+                  child: const Text('H'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Discover');
+                  },
+                  child: const Text('D'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/NewCluck');
+                  },
+                  child: const Text('NC'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Search');
+                  },
+                  child: const Text('S'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/Notification');
+                  },
+                  child: const Text('N'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
