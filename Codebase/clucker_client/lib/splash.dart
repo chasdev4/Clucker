1de5ddb0020d13e7ed8ed94b8074d0d5ed2e18e0
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-07 09:39:03 -0800

    CLUCK-324 Added font size

--- Codebase/clucker_client/lib/splash.dart
+++ Codebase/clucker_client/lib/splash.dart
@@ -5,28 +5,30 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Image(
-            image: AssetImage('assets/icons/clucker-icon.png'),
-            width: 256,
-            height: 256,
-          ),
-          Text(
-            "Clucker",
-            style:
-                TextStyle(
-                    color: Colors.black,
-                    decoration: TextDecoration.none,
-                    fontFamily: 'OpenSans',
-                    fontWeight: FontWeight.w700
-                ),
-          )
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('assets/icons/clucker-icon.png'),
+              width: 256,
+              height: 256,
+            ),
+            Text(
+              "Clucker",
+
+              style:
+              TextStyle(
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
