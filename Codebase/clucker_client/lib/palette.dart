60b68c49a76c5a8b85a50c16eee693cb17fa3897
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-07 10:37:41 -0800

    CLUCK-95 Created palette.dart

---
+++ Codebase/clucker_client/lib/palette.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// Header Dividers, Hyperlinks, Active Icons, Active Toggle Button, Solid Button Background.
+// (Default Theme) Secondary Button Border and Secondary Button Text
+const cluckerRed = Color.fromARGB(255, 255, 87, 87);
+
+// Solid Button Text
+// (Default Theme) Background, Text Field Background
+// (Dark Mode) Text, Inactive Icon, Inactive Text, Secondary Button Text
+const white = Colors.white;
+
+//#region Default Theme
+
+// Text, Inactive Icon, Inactive Text
+const black = Colors.black;
+
+// Standard Dividers, Inactive Toggle Button, Button Widget (on click)
+const mercuryGray = Color.fromARGB(255, 230, 230, 230);
+
+// Comments and Settings Background Color
+const porcelain = Color.fromARGB(255, 242, 242, 242);
+
+//#endregion
+
+
+//#region Dark Mode
+
+// Background
+const gunmetal = Color.fromARGB(255, 44, 48, 54);
+
+// Standard Dividers, Text Field Background, Secondary Button Background
+const cloudyGrey = Color.fromARGB(255, 104, 106, 108);
+
+// Comments, Settings, Button Widget (on click)
+const cinder = Color.fromARGB(255, 19, 24, 28);
+
+//#endregion
