6ebb24aca4600222d8e49b783d7b673eaaae5308
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-07 12:08:57 -0800

    bugfix-scaffold background color is invalid

--- Codebase/clucker_client/lib/main.dart
+++ Codebase/clucker_client/lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
         fontFamily: 'OpenSans'
-        scaffoldBackgroundColor: Colors.white,
       ),
       home: const SplashScreen(),
     );
