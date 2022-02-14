5094f91d036cf3ec3123d5161e96ab4dc499f345
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-14 12:56:06 -0800

    bugfix-main-test_screen-update path variables

--- Codebase/clucker_client/lib/main.dart
+++ Codebase/clucker_client/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:clucker_client/splash.dart';
 import 'package:clucker_client/test_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:clucker_client/palette.dart';
+import 'package:clucker_client/components/palette.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: cluckerRed,
         primaryColor: cluckerRed.shade900,
       ),
-      home: const TestScreen(),
+      home: const SplashScreen(),
     );
   }
 }
