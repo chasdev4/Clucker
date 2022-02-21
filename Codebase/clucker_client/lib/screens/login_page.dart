6a3ffd2ff80bd6385d7b84521b97d07962d8ed87
Zachary Culpepper <zculp4579@gmail.com>
2022-02-21 09:06:04 -0600

    CLUCK-102 #4
    Redesign and refactor

--- Codebase/clucker_client/lib/screens/login_page.dart
+++ Codebase/clucker_client/lib/screens/login_page.dart
@@ -7,35 +7,29 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Clucker',
-          style: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget> [
+              Image(
+                image: AssetImage(
+                    'assets/icons/clucker_logo_256x256.png'
+                ),
+              ),
+              Text(
+                'Clucker',
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 45,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+            ],
           ),
-        ),
-      ),
-      body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            Column(
-              children: const <Widget> [
-                SizedBox(
-                  height: 25,
-                ),
-                CircleAvatar(
-                  radius: 125,
-                  backgroundImage: AssetImage('assets/icons/clucker_logo_1024x1024.png'),
-                ),
-                Text('Login'),
-              ],
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
