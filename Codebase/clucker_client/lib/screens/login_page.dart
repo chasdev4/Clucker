9737f547b6914a54f8ab40cd99463856fd8bc65d
Zachary Culpepper <zculp4579@gmail.com>
2022-02-21 10:00:41 -0600

    CLUCK-102
    Added Text fields with input validation

--- Codebase/clucker_client/lib/screens/login_page.dart
+++ Codebase/clucker_client/lib/screens/login_page.dart
@@ -7,18 +7,36 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: LogInForm(),
+    );
+  }
+}
+
+class LogInForm extends StatefulWidget {
+  const LogInForm({Key? key}) : super(key: key);
+
+  @override
+  _LogInFormState createState() => _LogInFormState();
+}
+
+class _LogInFormState extends State<LogInForm> {
+
+  final _logInFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _logInFormKey,
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
+        children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget> [
-              Image(
-                image: AssetImage(
-                    'assets/icons/clucker_logo_256x256.png'
-                ),
+            children: <Widget>[
+              const Image(
+                image: AssetImage('assets/icons/clucker_logo_256x256.png'),
               ),
-              Text(
+              const Text(
                 'Clucker',
                 style: TextStyle(
                   fontFamily: 'OpenSans',
@@ -26,7 +44,56 @@ class LoginPage extends StatelessWidget {
                   fontWeight: FontWeight.w700,
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Username!';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter Username...',
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Password!';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter Password...',
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_logInFormKey.currentState!.validate()) {
 
+                  }
+                },
+                child: const Text('Log-In'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Sign-Up'),
+              ),
             ],
           ),
         ],
