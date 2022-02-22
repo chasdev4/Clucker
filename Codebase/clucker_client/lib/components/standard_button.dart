2d3e4cfadf1e606402f956fba254b939a6144cb3
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-22 10:38:16 -0800

    CLUCK-350 Refactored StandardButton

--- Codebase/clucker_client/lib/components/standard_button.dart
+++ Codebase/clucker_client/lib/components/standard_button.dart
@@ -1,88 +1,101 @@
 import 'package:flutter/material.dart';
 
-enum StandardButtonProfile {
-  standard,
-  save,
-  cancel,
-  revert
-}
+enum StandardButtonProfile { standard, save, cancel, revert }
 
-enum StatefulButtonProfile {
-  follow,
-  followSmall,
-}
-
-class StatefulButton extends StatefulWidget {
-  const StatefulButton({Key? key}) : super(key: key);
-
-  @override
-  _StatefulButtonState createState() => _StatefulButtonState();
-}
-
-class _StatefulButtonState extends State<StatefulButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
+enum FollowButtonProfile { standard, small, block }
 
 class StandardButton extends StatelessWidget {
+  const StandardButton(
+      {Key? key,
+      required this.text,
+      required this.routeName,
+      required this.onPress,
+      this.isSecondary = false,
+      this.standardButtonProfile = StandardButtonProfile.standard})
+      : super(key: key);
 
   final String text;
   final String routeName;
   final Function onPress;
   final bool isSecondary;
-
-  final double width = 250;
-  final double height = 50;
-
-  const StandardButton({Key? key, required this.text,
-    required this.routeName, required this.onPress, this.isSecondary = false}) : super(key: key);
+  final StandardButtonProfile standardButtonProfile;
 
   @override
   Widget build(BuildContext context) {
-    var textColor = (isSecondary == true) ? Colors.red : Colors.white;
-    var buttonColor = (isSecondary == false) ? Colors.red : Colors.white;
-    return _ButtonFactory(text, routeName, onPress, isSecondary, width, height,
-        textColor, buttonColor);
-  }
-}
+    double horizontalPadding =
+        (standardButtonProfile == StandardButtonProfile.standard) ? 50 : 6;
 
-class _ButtonFactory extends StatelessWidget {
-  const _ButtonFactory(this.text, this.routeName, this.onPress, this.isSecondary, this.width,
-      this.height, this.textColor, this.buttonColor);
-
-  final String text;
-  final String routeName;
-  final Function onPress;
-  final bool isSecondary;
-  final double width;
-  final double height;
-  final Color textColor;
-  final Color buttonColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      child: ElevatedButton(
-        child: Text(
-          text,
-          style: TextStyle(
-              color: textColor, fontSize: 15, fontWeight: FontWeight.w500),
-        ),
-        style: ElevatedButton.styleFrom(
-          fixedSize: Size(width, height),
-          primary: buttonColor,
-          side: (isSecondary == true)
-              ? BorderSide(color: textColor, width: 2.5, style: BorderStyle.solid)
-              : const BorderSide(style: BorderStyle.none),
-        ),
-        onPressed: () {
-          onPress();
-        },
+    Color textColor = (isSecondary == true) ? Colors.red : Colors.white;
+    Color buttonColor = (isSecondary == false) ? Colors.red : Colors.white;
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      SizedBox(
+        width: horizontalPadding,
       ),
-      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: horizontalPadding),
-    );
+      Expanded(
+        child: ElevatedButton(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: textColor, fontSize: 15, fontWeight: FontWeight.w500),
+          ),
+          style: ElevatedButton.styleFrom(
+            primary: buttonColor,
+            side: (isSecondary == true)
+                ? BorderSide(
+                    color: textColor, width: 2.5, style: BorderStyle.solid)
+                : const BorderSide(style: BorderStyle.none),
+          ),
+          onPressed: () {
+            onPress();
+          },
+        ),
+      ),
+      SizedBox(
+        width: horizontalPadding,
+      ),
+    ]);
   }
 }
+
+// class FollowButton extends StatefulWidget {
+//   const FollowButton({Key? key}) : super(key: key);
+//
+//   final String text;
+//   final String routeName;
+//   final Function onPress;
+//   final bool isSecondary;
+//   final Color textColor;
+//   final Color buttonColor;
+//
+//   final double width = 250;
+//   final double height = 50;
+//
+//   @override
+//   _FollowButtonState createState() => _FollowButtonState();
+// }
+//
+// class _FollowButtonState extends State<FollowButton> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       child: ElevatedButton(
+//         child: Text(
+//           text,
+//           style: TextStyle(
+//               color: textColor, fontSize: 15, fontWeight: FontWeight.w500),
+//         ),
+//         style: ElevatedButton.styleFrom(
+//           fixedSize: Size(width, height),
+//           primary: buttonColor,
+//           side: (isSecondary == true)
+//               ? BorderSide(color: textColor, width: 2.5, style: BorderStyle.solid)
+//               : const BorderSide(style: BorderStyle.none),
+//         ),
+//         onPressed: () {
+//           onPress();
+//         },
+//       ),
+//       padding: const EdgeInsets.symmetric(vertical: 6, horizontal: horizontalPadding),
+//     );;
+//   }
+// }
