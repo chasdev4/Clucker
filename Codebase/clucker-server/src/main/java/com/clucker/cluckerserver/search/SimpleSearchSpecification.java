2c99b941bfc87b958407d133b76e16c8dd4ac2c1
lyabut07 <lyabut@student.fullsail.edu>
2022-02-23 23:01:17 -0700

    CLUCK-81 Add search specification.

---
+++ Codebase/clucker-server/src/main/java/com/clucker/cluckerserver/search/SimpleSearchSpecification.java
@@ -0,0 +1,34 @@
+package com.clucker.cluckerserver.search;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.Attribute;
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Getter
+public class SimpleSearchSpecification<T> implements Specification<T> {
+
+    private final String searchString;
+
+    @Override
+    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        String[] searchTerms = getSearchString().split("[\\s,]");
+
+        Predicate[] predicates = Arrays.stream(searchTerms)
+                .map(String::toLowerCase)
+                .flatMap(searchTerm -> root.getModel().getAttributes().stream()
+                        .filter(attribute -> attribute.getJavaType() == String.class)
+                        .map(Attribute::getName)
+                        .map(attributeName -> cb.like(cb.lower(root.get(attributeName)), "%" + searchTerm + "%")))
+                .toArray(Predicate[]::new);
+
+        return cb.or(predicates);
+    }
+}
