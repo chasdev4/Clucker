f4303eb5ceb354d53835255c8513eee66c7079a1
CFHoldren <CFHoldren@student.fullsail.edu>
2022-02-25 15:08:11 -0800

    CLUCK-54 Added some functionality to validation fields

--- Codebase/clucker_client/lib/screens/email_signup_page.dart
+++ Codebase/clucker_client/lib/screens/email_signup_page.dart
@@ -45,97 +45,135 @@ class _EmailFormState extends State<EmailForm> {
     return Form(
       key: _emailFormKey,
       child: Container(
-      alignment: Alignment.center,
+        alignment: Alignment.center,
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 125),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width - 50,
-              child: const Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 36,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 125),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width - 50,
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 6,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width - 100,
-            child: const Text(
-              'Enter your email and password...',
-              style: TextStyle(
-                fontFamily: 'OpenSans',
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              child: const Text(
+                'Enter your email and password...',
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextBox(
-            textBoxProfile: TextBoxProfile.emailFieldSignUp,
-            controller: emailController,
-            onEditingComplete: (){},
-            onChanged: (){},
-          ),
-          TextBox(
-            textBoxProfile: TextBoxProfile.passwordFieldSignUp,
-            controller: firstPasswordController,
-            onEditingComplete: () {},
-            onChanged: (){},
-          ),
-          TextBox(
-            textBoxProfile: TextBoxProfile.passwordFieldConfirmSignUp,
-            controller: secondPasswordController,
-            onEditingComplete: () {},
-            onChanged: (){},
-          ),
-          StandardButton(
-            text: 'Sign-Up',
-            routeName: '',
-            onPress: () async {
-              email = emailController.text;
+            const SizedBox(
+              height: 15,
+            ),
+            TextBox(
+                textBoxProfile: TextBoxProfile.emailFieldSignUp,
+                controller: emailController,
+                onEditingComplete: () async {
+                  if (_emailFormKey.currentState!.validate()) {}
+                  email = emailController.text;
+                  return await userService.emailAvailable(email);
+                },
+                onChanged: () async {
+                  if (_emailFormKey.currentState!.validate()) {}
+                  email = emailController.text;
+                  return await userService.emailAvailable(email);
+                }),
+            TextBox(
+              textBoxProfile: TextBoxProfile.passwordFieldSignUp,
+              controller: firstPasswordController,
+              onEditingComplete: () {},
+              onChanged: () {},
+            ),
+            TextBox(
+              textBoxProfile: TextBoxProfile.passwordFieldConfirmSignUp,
+              controller: secondPasswordController,
+              onEditingComplete: () {
+                if (firstPasswordController.text ==
+                    secondPasswordController.text) {
+                  return true;
+                }
+                else {
+                  return false;
+                }
+              },
+              onChanged: () {
+                if (firstPasswordController.text ==
+                    secondPasswordController.text) {
+                  return true;
+                }
+                else {
+                  return false;
+                }
+              },
+            ),
+            StandardButton(
+              text: 'Sign-Up',
+              routeName: '',
+              onPress: () async {
+                email = emailController.text;
 
-              if (firstPasswordController.text ==
-                  secondPasswordController.text) {
-                password = firstPasswordController.text;
+                if (firstPasswordController.text ==
+                    secondPasswordController.text) {
+                  password = firstPasswordController.text;
 
-                UserRegistration userRegistration = UserRegistration(
-                    username: widget.username,
-                    password: password,
-                    email: email);
+                  UserRegistration userRegistration = UserRegistration(
+                      username: widget.username,
+                      password: password,
+                      email: email);
 
-                Response response =
-                    await userService.registerUser(userRegistration);
+                  Response response =
+                      await userService.registerUser(userRegistration);
 
-                if (response.statusCode == 201) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Account Created'),
-                      content: const Text('Start Clucking!'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'OK'),
-                          child: const Text('OK'),
-                        ),
-                      ],
-                    ),
-                  );
+                  if (response.statusCode == 201) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Account Created'),
+                        content: const Text('Start Clucking!'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('ERROR'),
+                        content: Text(response.body),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
                 } else {
                   showDialog(
                     context: context,
                     builder: (BuildContext context) => AlertDialog(
-                      title: const Text('ERROR'),
-                      content: Text(response.body),
+                      title: const Text('Password Conflict'),
+                      content: const Text('Passwords must match!'),
                       actions: <Widget>[
                         TextButton(
                           onPressed: () => Navigator.pop(context, 'OK'),
@@ -145,33 +183,19 @@ class _EmailFormState extends State<EmailForm> {
                     ),
                   );
                 }
-              } else {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('Password Conflict'),
-                    content: const Text('Passwords must match!'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => Navigator.pop(context, 'OK'),
-                        child: const Text('OK'),
-                      ),
-                    ],
-                  ),
-                );
-              }
-            },
-          ),
-          StandardButton(
-            text: 'Back',
-            routeName: '',
-            onPress: () {
-              Navigator.pop(context);
-            },
-            isSecondary: true,
-          ),
-        ],
-      ),),
+              },
+            ),
+            StandardButton(
+              text: 'Back',
+              routeName: '',
+              onPress: () {
+                Navigator.pop(context);
+              },
+              isSecondary: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
