f7ce79217e4177cd9e5d8b3866ed912cd8da55bd
CFHoldren <CFHoldren@student.fullsail.edu>
2022-03-01 22:31:32 -0800

    CLUCK-105 Revert changes to main

--- Codebase/clucker_client/lib/main.dart
+++ Codebase/clucker_client/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Palette.cluckerRed.toMaterialColor(),
         primaryColor: Palette.cluckerRed,
       ),
-      home: const EmailPage(username: 'username'),
+      home: const LoginPage(),
     );
   }
 }
