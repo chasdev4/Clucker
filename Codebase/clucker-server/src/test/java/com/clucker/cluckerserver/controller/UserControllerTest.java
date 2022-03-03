7be0ae0bf1a1e3197d281128951b8e0c086d4829
lyabut07 <lyabut@student.fullsail.edu>
2022-03-02 21:26:54 -0700

    Optimize imports.

--- Codebase/clucker-server/src/test/java/com/clucker/cluckerserver/controller/UserControllerTest.java
+++ Codebase/clucker-server/src/test/java/com/clucker/cluckerserver/controller/UserControllerTest.java
@@ -3,7 +3,7 @@ package com.clucker.cluckerserver.controller;
 import com.clucker.cluckerserver.annotation.IntegrationTest;
 import com.clucker.cluckerserver.dto.UserRegistration;
 import com.clucker.cluckerserver.dto.UserUpdateRequest;
-import com.clucker.cluckerserver.model.User;
+import com.clucker.cluckerserver.repository.model.User;
 import com.clucker.cluckerserver.api.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
