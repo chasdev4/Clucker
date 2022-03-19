7eb8cdb6e95bc084905091a9f14e8a039c8553e4
CFHoldren <CFHoldren@student.fullsail.edu>
2022-03-19 13:52:43 -0700

    CLUCK-389 created edit_profile_page.dart

---
+++ Codebase/clucker_client/lib/screens/edit_profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:clucker_client/screens/profile_page.dart';
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({Key? key, required this.profileData}) : super(key: key);
+
+  final ProfileData profileData;
+
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
